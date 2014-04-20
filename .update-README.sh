8607e5cbda1cff186f62792b81f8952778492782
Tim 'mithro' Ansell <mithro@mithis.com>
2014-04-20 16:33:26 +1000

    Update script now correctly fixes the wiki links.

--- .update-README.sh
+++ .update-README.sh
@@ -3,5 +3,6 @@
 # Download the README from the HDMI2USB wiki
 
 wget -O README.md.new https://raw.github.com/wiki/timvideos/HDMI2USB/Firmware.md
+sed -i -e's-\[\[\([^\]*\)]]-(\1)[http://github.com/timvideos/HDMI2USB/wiki/\1]-' README.md.new
 diff -u README.md README.md.new
 mv README.md.new README.md
