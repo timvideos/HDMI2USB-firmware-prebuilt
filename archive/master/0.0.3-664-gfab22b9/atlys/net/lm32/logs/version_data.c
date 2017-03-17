b18fe1d5b2a2bfe860b72eecb2ec7a95c6b73686
Tim 'mithro' Ansell <mithro@mithis.com>
2017-03-17 00:21:12 +0000

    Travis build #656 of master/0.0.3-664-gfab22b9 for PLATFORM=atlys TARGET=net CPU=lm32

---
+++ archive/master/0.0.3-664-gfab22b9/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "fab22b9b3252d5ea8b7cf1cfd1d14d86929fd82e";
+const char* git_branch = "master";
+const char* git_describe = "0.0.3-664-gfab22b9-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
