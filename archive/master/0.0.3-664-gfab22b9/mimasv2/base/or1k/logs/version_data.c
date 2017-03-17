b5109c8c003eb503116a804030014157d4363452
Tim 'mithro' Ansell <mithro@mithis.com>
2017-03-17 00:49:51 +0000

    Travis build #656 of master/0.0.3-664-gfab22b9 for PLATFORM=mimasv2 TARGET=base CPU=or1k

---
+++ archive/master/0.0.3-664-gfab22b9/mimasv2/base/or1k/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "fab22b9b3252d5ea8b7cf1cfd1d14d86929fd82e";
+const char* git_branch = "master";
+const char* git_describe = "0.0.3-664-gfab22b9-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
