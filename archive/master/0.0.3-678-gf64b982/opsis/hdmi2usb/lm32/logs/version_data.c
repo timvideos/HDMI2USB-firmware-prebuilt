1da188ffbd35a5e211f32557d59330f3bccb4b7b
Tim Ansell <mithro@mithis.com>
2017-03-17 12:21:33 +0000

    Travis build #658 of master/0.0.3-678-gf64b982 for PLATFORM=opsis TARGET=hdmi2usb CPU=lm32

---
+++ archive/master/0.0.3-678-gf64b982/opsis/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "f64b982e72fca44445ca82e591ce0842261859c7";
+const char* git_branch = "master";
+const char* git_describe = "0.0.3-678-gf64b982-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
