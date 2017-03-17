fdeffab6fc04ecc2c43014864e535737074daeae
Tim Ansell <mithro@mithis.com>
2017-03-17 11:47:57 +0000

    Travis build #658 of master/0.0.3-678-gf64b982 for PLATFORM=minispartan6 TARGET=base CPU=lm32

---
+++ archive/master/0.0.3-678-gf64b982/minispartan6/base/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_MINISPARTAN6
+#error "Version mismatch - PLATFORM_MINISPARTAN6 not defined!"
+#endif
+const char* board = "minispartan6";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "f64b982e72fca44445ca82e591ce0842261859c7";
+const char* git_branch = "master";
+const char* git_describe = "0.0.3-678-gf64b982-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
