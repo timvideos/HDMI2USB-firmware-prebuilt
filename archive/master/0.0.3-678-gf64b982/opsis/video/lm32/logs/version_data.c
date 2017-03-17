1fafc92eb0bbc9ede4619050bb5ef2d0bcdb6e6c
Tim Ansell <mithro@mithis.com>
2017-03-17 12:03:58 +0000

    Travis build #658 of master/0.0.3-678-gf64b982 for PLATFORM=opsis TARGET=video CPU=lm32

---
+++ archive/master/0.0.3-678-gf64b982/opsis/video/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "f64b982e72fca44445ca82e591ce0842261859c7";
+const char* git_branch = "master";
+const char* git_describe = "0.0.3-678-gf64b982-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
