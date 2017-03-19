2a4e0629f68a3f3fb1c447945b0a36b9cca4f025
Tim Ansell <mithro@mithis.com>
2017-03-19 08:18:20 +0000

    Travis build #660 of master/0.0.3-681-g97b9aaa for PLATFORM=atlys TARGET=base CPU=lm32\nFrom https://github.com/timvideos/HDMI2USB-litex-firmware/tree/97b9aaa6c16b4c921d8e7d20ad079ef920788aa8\n

---
+++ archive/master/0.0.3-681-g97b9aaa/atlys/base/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "97b9aaa6c16b4c921d8e7d20ad079ef920788aa8";
+const char* git_branch = "master";
+const char* git_describe = "0.0.3-681-g97b9aaa-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
