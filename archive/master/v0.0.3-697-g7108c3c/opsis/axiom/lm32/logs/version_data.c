83a5670b8be6429de1ef8af9cc6d01e3206c5027
Tim 'mithro' Ansell <mithro@mithis.com>
2017-04-02 04:33:53 +0000

    Travis build #675 of master/v0.0.3-697-g7108c3c for PLATFORM=opsis TARGET=axiom CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/7108c3c31abf8f244bcd5f88577ebdbc564a3d2a

---
+++ archive/master/v0.0.3-697-g7108c3c/opsis/axiom/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_AXIOM
+#error "Version mismatch - TARGET_AXIOM not defined!"
+#endif
+const char* target = "axiom";
+
+const char* git_commit = "7108c3c31abf8f244bcd5f88577ebdbc564a3d2a";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.3-697-g7108c3c-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
