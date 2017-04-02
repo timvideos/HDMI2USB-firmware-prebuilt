1f43224429f4d72722b6d190001dacf5afceeb88
Tim 'mithro' Ansell <mithro@mithis.com>
2017-04-02 11:59:39 +0000

    Travis build #677 of master/v0.0.3-699-g286bba2 for PLATFORM=opsis TARGET=hdmi2usb CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/286bba20570d4b6da433599ef9c022bf99808d44

---
+++ archive/master/v0.0.3-699-g286bba2/opsis/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "286bba20570d4b6da433599ef9c022bf99808d44";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.3-699-g286bba2-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
