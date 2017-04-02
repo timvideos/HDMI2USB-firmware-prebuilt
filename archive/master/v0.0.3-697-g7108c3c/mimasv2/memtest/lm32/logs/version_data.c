17a9a96d26c401057e25f4c5ce9c8e7b6b1b1f45
Tim 'mithro' Ansell <mithro@mithis.com>
2017-04-02 05:00:43 +0000

    Travis build #675 of master/v0.0.3-697-g7108c3c for PLATFORM=mimasv2 TARGET=memtest CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/7108c3c31abf8f244bcd5f88577ebdbc564a3d2a

---
+++ archive/master/v0.0.3-697-g7108c3c/mimasv2/memtest/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_MEMTEST
+#error "Version mismatch - TARGET_MEMTEST not defined!"
+#endif
+const char* target = "memtest";
+
+const char* git_commit = "7108c3c31abf8f244bcd5f88577ebdbc564a3d2a";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.3-697-g7108c3c-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
