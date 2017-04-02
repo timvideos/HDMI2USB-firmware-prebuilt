c5e8cf81f7b47d37fe677b0888125e501971400e
Tim 'mithro' Ansell <mithro@mithis.com>
2017-04-02 11:47:50 +0000

    Travis build #677 of master/v0.0.3-699-g286bba2 for PLATFORM=mimasv2 TARGET=base CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/286bba20570d4b6da433599ef9c022bf99808d44

---
+++ archive/master/v0.0.3-699-g286bba2/mimasv2/base/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "286bba20570d4b6da433599ef9c022bf99808d44";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.3-699-g286bba2-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
