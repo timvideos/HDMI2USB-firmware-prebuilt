229bac1c638fc8fd8fc78e644e25f13caf3c38a3
Florent Kermarrec <florent@enjoy-digital.fr>
2017-04-19 10:10:03 +0000

    Travis build #683 of master/v0.0.3-710-g947321b for PLATFORM=mimasv2 TARGET=base CPU=or1k
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/947321b97655ea02e837d9736736d4a2d9550aa2

---
+++ archive/master/v0.0.3-710-g947321b/mimasv2/base/or1k/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "947321b97655ea02e837d9736736d4a2d9550aa2";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.3-710-g947321b-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
