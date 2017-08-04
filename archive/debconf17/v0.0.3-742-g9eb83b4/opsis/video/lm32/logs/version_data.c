a53443e059201d64ef50b67cff330c9327ff7a4c
Stefano Rivera <stefano@rivera.za.net>
2017-08-04 16:18:36 +0000

    Travis build #712 of debconf17/v0.0.3-742-g9eb83b4 for PLATFORM=opsis TARGET=video CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/9eb83b4a6645950a13e466893ff20b9683c4ed15

---
+++ archive/debconf17/v0.0.3-742-g9eb83b4/opsis/video/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "9eb83b4a6645950a13e466893ff20b9683c4ed15";
+const char* git_branch = "debconf17";
+const char* git_describe = "v0.0.3-742-g9eb83b4-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
