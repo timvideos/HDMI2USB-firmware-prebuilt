da1daa62a1bc1f6100cf43fd45a5ad7dc30437bd
Stefano Rivera <stefano@rivera.za.net>
2017-08-04 01:04:58 +0000

    Travis build #710 of debconf17/v0.0.3-741-g7fcbe0c for PLATFORM=atlys TARGET=net CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/7fcbe0c45aa68ef2f38da27b7e91a847e2ad7930

---
+++ archive/debconf17/v0.0.3-741-g7fcbe0c/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "7fcbe0c45aa68ef2f38da27b7e91a847e2ad7930";
+const char* git_branch = "debconf17";
+const char* git_describe = "v0.0.3-741-g7fcbe0c-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
