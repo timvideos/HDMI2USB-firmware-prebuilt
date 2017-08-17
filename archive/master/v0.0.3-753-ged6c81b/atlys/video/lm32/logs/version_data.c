a31079c1013c792ee93190e505a3bb8b91a3f6d1
Tim Ansell <mithro@mithis.com>
2017-08-17 03:51:33 +0000

    Travis build #729 of master/v0.0.3-753-ged6c81b for PLATFORM=atlys TARGET=video CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/ed6c81b011ec82e1782370e4958f22d60bdd73c1

---
+++ archive/master/v0.0.3-753-ged6c81b/atlys/video/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "ed6c81b011ec82e1782370e4958f22d60bdd73c1";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.3-753-ged6c81b-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     M ../../../../third_party/litepcie\r\n"
+   "     M ../../../../third_party/litesata\r\n"
+   "     M ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
