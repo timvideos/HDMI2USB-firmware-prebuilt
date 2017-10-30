df13942b387d5bbde6ed7dcda5b1a557dae7dca5
Tim Ansell <mithro@mithis.com>
2017-10-30 21:35:33 +0000

    Travis build #815 of master/v0.0.3-1035-ge653dd5 for PLATFORM=opsis TARGET=base CPU=or1k
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/e653dd538ee930ddc0f37810de24e347cc2483c3

---
+++ archive/master/v0.0.3-1035-ge653dd5/opsis/base/or1k/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "e653dd538ee930ddc0f37810de24e347cc2483c3";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.3-1035-ge653dd5-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     ? ../../../../third_party/litepcie\r\n"
+   "     ? ../../../../third_party/litesata\r\n"
+   "     ? ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
