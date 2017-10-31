6ffc386c6737854ccc244d5d0039a6656342a15c
Tim Ansell <mithro@mithis.com>
2017-10-31 00:43:06 +0000

    Travis build #817 of master/v0.0.3-1045-gccb30b9 for PLATFORM=netv2 TARGET=video CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/ccb30b98cf86dfd13f619c69ac51f69404e43866

---
+++ archive/master/v0.0.3-1045-gccb30b9/netv2/video/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "ccb30b98cf86dfd13f619c69ac51f69404e43866";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.3-1045-gccb30b9-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     ? ../../../../third_party/litepcie\r\n"
+   "     ? ../../../../third_party/litesata\r\n"
+   "     ? ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
