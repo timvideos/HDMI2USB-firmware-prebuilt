7a3e0565da94b7bd266c6f67b7debfd7133ee592
Tim Ansell <mithro@mithis.com>
2017-10-31 20:44:21 +0000

    Travis build #825 of master/v0.0.3-1050-gffac402 for PLATFORM=atlys TARGET=net CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/ffac4025ce62b29a897ef51fd9452ed83a387c73

---
+++ archive/master/v0.0.3-1050-gffac402/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "ffac4025ce62b29a897ef51fd9452ed83a387c73";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.3-1050-gffac402-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     ? ../../../../third_party/litepcie\r\n"
+   "     ? ../../../../third_party/litesata\r\n"
+   "     ? ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
