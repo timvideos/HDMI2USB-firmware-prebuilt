0d54c20b5f5b0df36ec7cbbfe3e6729ed86e817c
Tim Ansell <mithro@mithis.com>
2017-12-02 08:06:17 +0000

    Travis build #832 of master/v0.0.3-1058-g0c06257 for PLATFORM=pipistrello TARGET=base CPU=or1k
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/0c062574e57dd513165a8188b30a41918e1ff1be

---
+++ archive/master/v0.0.3-1058-g0c06257/pipistrello/base/or1k/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "0c062574e57dd513165a8188b30a41918e1ff1be";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.3-1058-g0c06257-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     ? ../../../../third_party/litepcie\r\n"
+   "     ? ../../../../third_party/litesata\r\n"
+   "     ? ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
