23b5e62691a51d68628159c280380ef212dea0a3
Tim 'mithro' Ansell <mithro@mithis.com>
2017-12-03 01:23:28 +0000

    Travis build #859 of travis-df-h/v0.0.3-1071-g98f57ba for PLATFORM=atlys TARGET=net CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/98f57bafbc38b9f7b15b90e9e73266e6d4401188

---
+++ archive/travis-df-h/v0.0.3-1071-g98f57ba/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "98f57bafbc38b9f7b15b90e9e73266e6d4401188";
+const char* git_branch = "travis-df-h";
+const char* git_describe = "v0.0.3-1071-g98f57ba-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     ? ../../../../third_party/litepcie\r\n"
+   "     ? ../../../../third_party/litesata\r\n"
+   "     ? ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
