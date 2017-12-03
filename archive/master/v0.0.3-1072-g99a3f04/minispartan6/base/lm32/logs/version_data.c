fd0148dab66e62c16bdfb3d5ddf80544c09de797
Tim Ansell <mithro@mithis.com>
2017-12-03 03:47:53 +0000

    Travis build #862 of master/v0.0.3-1072-g99a3f04 for PLATFORM=minispartan6 TARGET=base CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/99a3f04a4ebd864c879bacfeff660c43118b8aff

---
+++ archive/master/v0.0.3-1072-g99a3f04/minispartan6/base/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_MINISPARTAN6
+#error "Version mismatch - PLATFORM_MINISPARTAN6 not defined!"
+#endif
+const char* board = "minispartan6";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "99a3f04a4ebd864c879bacfeff660c43118b8aff";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.3-1072-g99a3f04-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     ? ../../../../third_party/litepcie\r\n"
+   "     ? ../../../../third_party/litesata\r\n"
+   "     ? ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
