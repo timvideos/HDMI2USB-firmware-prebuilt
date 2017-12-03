edff0000999fe8e37c898c43db8d2d177bf852bb
Tim 'mithro' Ansell <mithro@mithis.com>
2017-12-03 03:27:54 +0000

    Travis build #861 of travis-df-h/v0.0.3-1072-ge5d83e7 for PLATFORM=arty TARGET=net CPU=or1k
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/e5d83e7c04f116924ce58e6e073cff80c92d126b

---
+++ archive/travis-df-h/v0.0.3-1072-ge5d83e7/arty/net/or1k/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "e5d83e7c04f116924ce58e6e073cff80c92d126b";
+const char* git_branch = "travis-df-h";
+const char* git_describe = "v0.0.3-1072-ge5d83e7-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     ? ../../../../third_party/litepcie\r\n"
+   "     ? ../../../../third_party/litesata\r\n"
+   "     ? ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
