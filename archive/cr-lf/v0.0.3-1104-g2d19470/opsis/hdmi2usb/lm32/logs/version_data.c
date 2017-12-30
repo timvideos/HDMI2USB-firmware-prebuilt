7a451a5ccd42ab43fab96d0a58813ffdaf4c7f56
Tim 'mithro' Ansell <me@mith.ro>
2017-12-30 18:27:34 +0000

    Travis build #900 of cr-lf/v0.0.3-1104-g2d19470 for PLATFORM=opsis TARGET=hdmi2usb CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/2d1947034bc0f635602764fc78ca3f503476b674

---
+++ archive/cr-lf/v0.0.3-1104-g2d19470/opsis/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "2d1947034bc0f635602764fc78ca3f503476b674";
+const char* git_branch = "cr-lf";
+const char* git_describe = "v0.0.3-1104-g2d19470";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
