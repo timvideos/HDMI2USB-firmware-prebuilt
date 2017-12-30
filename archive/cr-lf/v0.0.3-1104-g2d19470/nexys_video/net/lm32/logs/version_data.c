2fe6fc2d70183979b80f544a754b164fe543942a
Tim 'mithro' Ansell <me@mith.ro>
2017-12-30 16:28:37 +0000

    Travis build #900 of cr-lf/v0.0.3-1104-g2d19470 for PLATFORM=nexys_video TARGET=net CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/2d1947034bc0f635602764fc78ca3f503476b674

---
+++ archive/cr-lf/v0.0.3-1104-g2d19470/nexys_video/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "2d1947034bc0f635602764fc78ca3f503476b674";
+const char* git_branch = "cr-lf";
+const char* git_describe = "v0.0.3-1104-g2d19470";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
