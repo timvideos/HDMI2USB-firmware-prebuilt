5e08692ca53b1df275301e1d96488ad62bcc1db2
Tim 'mithro' Ansell <me@mith.ro>
2017-12-30 16:02:47 +0000

    Travis build #900 of cr-lf/v0.0.3-1104-g2d19470 for PLATFORM=arty TARGET=net CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/2d1947034bc0f635602764fc78ca3f503476b674

---
+++ archive/cr-lf/v0.0.3-1104-g2d19470/arty/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "2d1947034bc0f635602764fc78ca3f503476b674";
+const char* git_branch = "cr-lf";
+const char* git_describe = "v0.0.3-1104-g2d19470";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
