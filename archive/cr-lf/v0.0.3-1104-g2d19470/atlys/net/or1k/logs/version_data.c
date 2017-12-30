60d73f0f6661e59b1c3f451558718631140a946e
Tim 'mithro' Ansell <me@mith.ro>
2017-12-30 16:55:57 +0000

    Travis build #900 of cr-lf/v0.0.3-1104-g2d19470 for PLATFORM=atlys TARGET=net CPU=or1k
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/2d1947034bc0f635602764fc78ca3f503476b674

---
+++ archive/cr-lf/v0.0.3-1104-g2d19470/atlys/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "2d1947034bc0f635602764fc78ca3f503476b674";
+const char* git_branch = "cr-lf";
+const char* git_describe = "v0.0.3-1104-g2d19470";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
