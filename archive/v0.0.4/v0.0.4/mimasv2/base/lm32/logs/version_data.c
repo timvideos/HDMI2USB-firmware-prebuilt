68649856527ea2f7287adddcd3fd2504defc26ac
Tim Ansell <me@mith.ro>
2018-01-04 09:32:20 +0000

    Travis build #938 of v0.0.4/v0.0.4 for PLATFORM=mimasv2 TARGET=base CPU=lm32
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/f24546cdbcef166c0b9c13e2617f1a07ef796fd3

---
+++ archive/v0.0.4/v0.0.4/mimasv2/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "f24546cdbcef166c0b9c13e2617f1a07ef796fd3";
+const char* git_branch = "heads/v0.0.4";
+const char* git_describe = "v0.0.4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
