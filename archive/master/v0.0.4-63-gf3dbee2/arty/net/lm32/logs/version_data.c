324413ef425316a8ecd79b15ba75216f8222720f
Tim Ansell <me@mith.ro>
2018-01-14 14:22:00 +0000

    Travis build #956 of master/v0.0.4-63-gf3dbee2 for PLATFORM=arty TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/f3dbee25885cad2b43fd63afbdc77b1511d9c2fa

---
+++ archive/master/v0.0.4-63-gf3dbee2/arty/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "f3dbee25885cad2b43fd63afbdc77b1511d9c2fa";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-63-gf3dbee2";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
