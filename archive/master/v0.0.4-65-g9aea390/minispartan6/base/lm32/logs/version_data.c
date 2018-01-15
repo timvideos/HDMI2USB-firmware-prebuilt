3bb4a6cfe1198f421aa463b3ac3e2d614c663872
Tim Ansell <me@mith.ro>
2018-01-15 06:01:16 +0000

    Travis build #958 of master/v0.0.4-65-g9aea390 for PLATFORM=minispartan6 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/9aea390e19babb6f356bd29e30638dbc4d3f375d

---
+++ archive/master/v0.0.4-65-g9aea390/minispartan6/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MINISPARTAN6
+#error "Version mismatch - PLATFORM_MINISPARTAN6 not defined!"
+#endif
+const char* board = "minispartan6";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "9aea390e19babb6f356bd29e30638dbc4d3f375d";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-65-g9aea390";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
