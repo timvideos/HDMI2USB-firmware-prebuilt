43953718c709c2b131a21ea1aea80ec6bb20e493
TimVideos.us Robot <robot@timvideos.us>
2018-01-19 02:15:05 -0800

    Travis build #974 of master/v0.0.4-96-gf170caa for PLATFORM=atlys TARGET=hdmi2usb CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/f170caa5dca8d0330332093f45931c6ef55c78b8

---
+++ archive/master/v0.0.4-96-gf170caa/atlys/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "f170caa5dca8d0330332093f45931c6ef55c78b8";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-96-gf170caa";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
