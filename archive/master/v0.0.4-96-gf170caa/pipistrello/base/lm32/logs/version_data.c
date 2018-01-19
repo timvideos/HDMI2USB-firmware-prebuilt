1ccf07eb47643b76ba581fc2b3900a91deb8c774
TimVideos.us Robot <robot@timvideos.us>
2018-01-19 00:35:34 -0800

    Travis build #974 of master/v0.0.4-96-gf170caa for PLATFORM=pipistrello TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/f170caa5dca8d0330332093f45931c6ef55c78b8

---
+++ archive/master/v0.0.4-96-gf170caa/pipistrello/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "f170caa5dca8d0330332093f45931c6ef55c78b8";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-96-gf170caa";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
