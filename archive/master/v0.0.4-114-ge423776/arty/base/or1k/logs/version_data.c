05a2d9833aa5c0983a6df584be85994c9df85c94
TimVideos.us Robot <robot@timvideos.us>
2018-01-20 18:00:10 -0800

    Travis build #982 of master/v0.0.4-114-ge423776 for PLATFORM=arty TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/e4237765c23ee89ece50bc366bb64bed558d0b27

---
+++ archive/master/v0.0.4-114-ge423776/arty/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "e4237765c23ee89ece50bc366bb64bed558d0b27";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-114-ge423776";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
