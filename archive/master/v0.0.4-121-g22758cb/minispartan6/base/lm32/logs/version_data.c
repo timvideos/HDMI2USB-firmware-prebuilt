cce68f978d58f1fac29de13ecc800de9b8012759
TimVideos.us Robot <robot@timvideos.us>
2018-01-24 21:39:37 -0800

    Travis build #985 of master/v0.0.4-121-g22758cb for PLATFORM=minispartan6 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/22758cb01ca08094cfa620159d1cbc4edb184343

---
+++ archive/master/v0.0.4-121-g22758cb/minispartan6/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MINISPARTAN6
+#error "Version mismatch - PLATFORM_MINISPARTAN6 not defined!"
+#endif
+const char* board = "minispartan6";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "22758cb01ca08094cfa620159d1cbc4edb184343";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-121-g22758cb";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
