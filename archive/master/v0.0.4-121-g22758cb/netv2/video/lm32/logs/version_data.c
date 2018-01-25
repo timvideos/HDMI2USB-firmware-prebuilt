298f90e5958d81cd8595497c91cd29c06eff4e85
TimVideos.us Robot <robot@timvideos.us>
2018-01-24 23:20:03 -0800

    Travis build #985 of master/v0.0.4-121-g22758cb for PLATFORM=netv2 TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/22758cb01ca08094cfa620159d1cbc4edb184343

---
+++ archive/master/v0.0.4-121-g22758cb/netv2/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "22758cb01ca08094cfa620159d1cbc4edb184343";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-121-g22758cb";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
