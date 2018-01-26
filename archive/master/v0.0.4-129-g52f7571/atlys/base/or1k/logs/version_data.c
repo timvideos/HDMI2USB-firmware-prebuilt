c1f2728c577914ba4aab73680e15780401dd4779
TimVideos.us Robot <robot@timvideos.us>
2018-01-25 19:05:45 -0800

    Travis build #990 of master/v0.0.4-129-g52f7571 for PLATFORM=atlys TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/52f75717ac6e7fee1db8d206dd77d4a542904bc7

---
+++ archive/master/v0.0.4-129-g52f7571/atlys/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "52f75717ac6e7fee1db8d206dd77d4a542904bc7";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-129-g52f7571";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
