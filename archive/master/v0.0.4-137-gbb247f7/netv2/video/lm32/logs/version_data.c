94f950375438ce3523b610ea7d743e2f78fa31f2
TimVideos.us Robot <robot@timvideos.us>
2018-02-08 11:27:41 -0800

    Travis build #998 of master/v0.0.4-137-gbb247f7 for PLATFORM=netv2 TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/bb247f772fcaf0952e849259f539dd3490f0eee1

---
+++ archive/master/v0.0.4-137-gbb247f7/netv2/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "bb247f772fcaf0952e849259f539dd3490f0eee1";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-137-gbb247f7";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
