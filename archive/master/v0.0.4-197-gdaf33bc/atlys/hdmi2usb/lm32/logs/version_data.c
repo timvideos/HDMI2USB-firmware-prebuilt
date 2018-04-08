2486dfdbbec45243d31aa9a8b8b00a5a4361616d
TimVideos.us Robot <robot@timvideos.us>
2018-04-08 12:29:37 -0700

    Travis build #1053 of master/v0.0.4-197-gdaf33bc for PLATFORM=atlys TARGET=hdmi2usb CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/daf33bc6a62433fde984b7166136585d82dd63de

---
+++ archive/master/v0.0.4-197-gdaf33bc/atlys/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "daf33bc6a62433fde984b7166136585d82dd63de";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-197-gdaf33bc";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
