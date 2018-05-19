18da9276d482e3e855aec63fd54b6f3f68c82a0e
TimVideos.us Robot <robot@timvideos.us>
2018-05-19 12:43:48 -0700

    Travis build #1072 of master/v0.0.4-239-gdcbcc05 for PLATFORM=atlys TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/dcbcc0576b3cd9395aefd4d8995207891d3d5f4f

---
+++ archive/master/v0.0.4-239-gdcbcc05/atlys/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "dcbcc0576b3cd9395aefd4d8995207891d3d5f4f";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-239-gdcbcc05";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
