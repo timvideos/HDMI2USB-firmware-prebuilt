5cfa9a7642ab4999c14b2c89977e0221bb29f943
TimVideos.us Robot <robot@timvideos.us>
2018-09-01 20:49:40 -0700

    Travis build #1117 of master/v0.0.4-283-ge260e6e for PLATFORM=nexys_video TARGET=net CPU=or1k.linux FIRMWARE=linux
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/e260e6e0fbc8dc3136d7e85acb0134debd924da5

---
+++ archive/master/v0.0.4-283-ge260e6e/nexys_video/net/or1k.linux/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "e260e6e0fbc8dc3136d7e85acb0134debd924da5";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-283-ge260e6e";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
