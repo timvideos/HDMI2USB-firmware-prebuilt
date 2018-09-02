2c68bc57a967bf803da2a29376174ce4881031ec
TimVideos.us Robot <robot@timvideos.us>
2018-09-01 19:22:57 -0700

    Travis build #1117 of master/v0.0.4-283-ge260e6e for PLATFORM=nexys_video TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/e260e6e0fbc8dc3136d7e85acb0134debd924da5

---
+++ archive/master/v0.0.4-283-ge260e6e/nexys_video/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "e260e6e0fbc8dc3136d7e85acb0134debd924da5";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-283-ge260e6e";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
