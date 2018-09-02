142a3ed0e35bd892e2b921dd4abf4d49416d3847
TimVideos.us Robot <robot@timvideos.us>
2018-09-01 19:37:40 -0700

    Travis build #1117 of master/v0.0.4-283-ge260e6e for PLATFORM=waxwing TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/e260e6e0fbc8dc3136d7e85acb0134debd924da5

---
+++ archive/master/v0.0.4-283-ge260e6e/waxwing/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_WAXWING
+#error "Version mismatch - PLATFORM_WAXWING not defined!"
+#endif
+const char* board = "waxwing";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "e260e6e0fbc8dc3136d7e85acb0134debd924da5";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-283-ge260e6e";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
