c72240c3f3a596f054bedd1ae8b276cb33fcc779
TimVideos.us Robot <robot@timvideos.us>
2018-09-18 21:43:11 -0700

    Travis build #1120 of master/v0.0.4-301-g3811d54 for PLATFORM=pipistrello TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/3811d5438cb0c3df3a2fa20f3b1d4cf0eb2b827b

---
+++ archive/master/v0.0.4-301-g3811d54/pipistrello/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "3811d5438cb0c3df3a2fa20f3b1d4cf0eb2b827b";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-301-g3811d54";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
