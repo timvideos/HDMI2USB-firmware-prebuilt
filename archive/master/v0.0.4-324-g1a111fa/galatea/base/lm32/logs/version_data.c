87ba5f1bbdce726ef2def52cdd59c789aa99764a
TimVideos.us Robot <robot@timvideos.us>
2018-09-27 01:19:13 -0700

    Travis build #1122 of master/v0.0.4-324-g1a111fa for PLATFORM=galatea TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/1a111fa023c97df7c9c88a60b057477a57bd63a0

---
+++ archive/master/v0.0.4-324-g1a111fa/galatea/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_GALATEA
+#error "Version mismatch - PLATFORM_GALATEA not defined!"
+#endif
+const char* board = "galatea";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "1a111fa023c97df7c9c88a60b057477a57bd63a0";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-324-g1a111fa";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
