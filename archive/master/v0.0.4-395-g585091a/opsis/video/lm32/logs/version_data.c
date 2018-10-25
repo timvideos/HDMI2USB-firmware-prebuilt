2795ba63cf9f03904bc1c19efa06f5ed3a31f5df
TimVideos.us Robot <robot@timvideos.us>
2018-10-25 16:17:29 -0700

    Travis build #1140 of master/v0.0.4-395-g585091a for PLATFORM=opsis TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/585091ad3d6529f6f86c35a62a3b74ddd8a3df49

---
+++ archive/master/v0.0.4-395-g585091a/opsis/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "585091ad3d6529f6f86c35a62a3b74ddd8a3df49";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-395-g585091a";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
