a55c2d6743f7b4c4e237ac7b99f289bdc2ec1e73
TimVideos.us Robot <robot@timvideos.us>
2018-10-26 09:48:45 -0700

    Travis build #1143 of master/v0.0.4-399-g33b53ba for PLATFORM=arty TARGET=base CPU=vexriscv FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/33b53baf4034c5f9377693c18f81f828f7c94901

---
+++ archive/master/v0.0.4-399-g33b53ba/arty/base/vexriscv/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "33b53baf4034c5f9377693c18f81f828f7c94901";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-399-g33b53ba";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
