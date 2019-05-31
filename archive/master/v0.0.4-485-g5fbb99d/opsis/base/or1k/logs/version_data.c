ba08f214821832de82386698bc812cf8060b5909
TimVideos.us Robot <robot@timvideos.us>
2019-05-31 06:15:56 -0500

    Travis build #1159 of master/v0.0.4-485-g5fbb99d for PLATFORM=opsis TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/5fbb99dbe4871bf0d82ccd61dc2952f8fb1e973a

---
+++ archive/master/v0.0.4-485-g5fbb99d/opsis/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "5fbb99dbe4871bf0d82ccd61dc2952f8fb1e973a";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-485-g5fbb99d";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
---
+++ archive/master/v0.0.4-485-g5fbb99d/opsis/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "5fbb99dbe4871bf0d82ccd61dc2952f8fb1e973a";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-485-g5fbb99d";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
