d69c04697a3cdd690e13c27117d2bc118ab8b0d6
TimVideos.us Robot <robot@timvideos.us>
2019-06-02 18:57:01 -0500

    Travis build #1162 of master/v0.0.4-489-geaab51f for PLATFORM=saturn TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/eaab51fd65f2c7276c32e6973ca2c86beb188fb0

---
+++ archive/master/v0.0.4-489-geaab51f/saturn/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_SATURN
+#error "Version mismatch - PLATFORM_SATURN not defined!"
+#endif
+const char* board = "saturn";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "eaab51fd65f2c7276c32e6973ca2c86beb188fb0";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-489-geaab51f";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
