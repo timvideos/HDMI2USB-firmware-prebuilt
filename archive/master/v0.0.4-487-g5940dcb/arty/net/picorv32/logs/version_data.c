92306582063277da84becb039a553f6bda498b4b
TimVideos.us Robot <robot@timvideos.us>
2019-06-02 17:08:10 -0500

    Travis build #1161 of master/v0.0.4-487-g5940dcb for PLATFORM=arty TARGET=net CPU=picorv32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/5940dcb7bbae596e70e632b46c382af724cbaf1e

---
+++ archive/master/v0.0.4-487-g5940dcb/arty/net/picorv32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "5940dcb7bbae596e70e632b46c382af724cbaf1e";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-487-g5940dcb";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
