6551a889d718104261b7bae28f0bcc8bdee1e0fe
TimVideos.us Robot <robot@timvideos.us>
2019-06-03 00:53:12 -0500

    Travis build #1162 of master/v0.0.4-489-geaab51f for PLATFORM=opsis TARGET=net CPU=or1k.linux FIRMWARE=linux
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/eaab51fd65f2c7276c32e6973ca2c86beb188fb0

---
+++ archive/master/v0.0.4-489-geaab51f/opsis/net/or1k.linux/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "eaab51fd65f2c7276c32e6973ca2c86beb188fb0";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-489-geaab51f";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
