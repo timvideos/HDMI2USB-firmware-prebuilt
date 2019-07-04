c257175094de5c5bb135e96fb219c9e1decfbb8d
TimVideos.us Robot <robot@timvideos.us>
2019-07-04 17:50:33 -0500

    Travis build #1165 of master/v0.0.4-502-g2bc1d19 for PLATFORM=opsis TARGET=net CPU=picorv32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/2bc1d19d4be7dc9b0b60bd0ca160d5ebbeb140b7

---
+++ archive/master/v0.0.4-502-g2bc1d19/opsis/net/picorv32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "2bc1d19d4be7dc9b0b60bd0ca160d5ebbeb140b7";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-502-g2bc1d19";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
