facf4e018a546d61f93228f85d493de32ae2b2c4
TimVideos.us Robot <robot@timvideos.us>
2019-07-05 01:31:44 -0500

    Travis build #1166 of master/v0.0.4-520-g4db1b0b for PLATFORM=neso TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/4db1b0b181d82c2eaefca5c8e7303d172f623616

---
+++ archive/master/v0.0.4-520-g4db1b0b/neso/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NESO
+#error "Version mismatch - PLATFORM_NESO not defined!"
+#endif
+const char* board = "neso";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "4db1b0b181d82c2eaefca5c8e7303d172f623616";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-520-g4db1b0b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
