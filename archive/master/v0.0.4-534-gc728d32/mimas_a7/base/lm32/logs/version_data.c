8687d702c959072d7af673a92459b75cf8ec96df
TimVideos.us Robot <robot@timvideos.us>
2019-07-07 02:47:14 -0500

    Travis build #1167 of master/v0.0.4-534-gc728d32 for PLATFORM=mimas_a7 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/c728d32244af525cfff776b13374689a0d7895de

---
+++ archive/master/v0.0.4-534-gc728d32/mimas_a7/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMAS_A7
+#error "Version mismatch - PLATFORM_MIMAS_A7 not defined!"
+#endif
+const char* board = "mimas_a7";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "c728d32244af525cfff776b13374689a0d7895de";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-534-gc728d32";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
