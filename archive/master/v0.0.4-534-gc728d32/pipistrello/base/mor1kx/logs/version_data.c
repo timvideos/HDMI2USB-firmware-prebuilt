d05aac5eba2281cf0cc086cc924aa7c332fa1a0e
TimVideos.us Robot <robot@timvideos.us>
2019-07-07 04:02:05 -0500

    Travis build #1167 of master/v0.0.4-534-gc728d32 for PLATFORM=pipistrello TARGET=base CPU=mor1kx FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/c728d32244af525cfff776b13374689a0d7895de

---
+++ archive/master/v0.0.4-534-gc728d32/pipistrello/base/mor1kx/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "c728d32244af525cfff776b13374689a0d7895de";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-534-gc728d32";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
