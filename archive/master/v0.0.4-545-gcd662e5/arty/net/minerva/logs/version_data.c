ab7b5d7e023c39a99640768c4cbe98cedb6d2063
TimVideos.us Robot <robot@timvideos.us>
2019-07-09 05:21:09 -0500

    Travis build #1170 of master/v0.0.4-545-gcd662e5 for PLATFORM=arty TARGET=net CPU=minerva FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/cd662e57b0d5450b605ac26cc4ca368f12d39a9e

---
+++ archive/master/v0.0.4-545-gcd662e5/arty/net/minerva/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "cd662e57b0d5450b605ac26cc4ca368f12d39a9e";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-545-gcd662e5";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
