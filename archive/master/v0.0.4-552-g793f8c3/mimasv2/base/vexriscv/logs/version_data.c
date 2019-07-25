b401ceca67257c76766cc9f1932d47c44de896bf
TimVideos.us Robot <robot@timvideos.us>
2019-07-25 01:03:48 -0500

    Travis build #1174 of master/v0.0.4-552-g793f8c3 for PLATFORM=mimasv2 TARGET=base CPU=vexriscv FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/793f8c3f2807f68ec6ffe8003d56e3cb0abd69be

---
+++ archive/master/v0.0.4-552-g793f8c3/mimasv2/base/vexriscv/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "793f8c3f2807f68ec6ffe8003d56e3cb0abd69be";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-552-g793f8c3";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
