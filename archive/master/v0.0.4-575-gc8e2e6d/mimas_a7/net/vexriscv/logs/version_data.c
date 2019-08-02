86ea004616b23317cb4de60f72ce50ea1b6fc78c
TimVideos.us Robot <robot@timvideos.us>
2019-08-02 06:47:57 -0500

    Travis build #1175 of master/v0.0.4-575-gc8e2e6d for PLATFORM=mimas_a7 TARGET=net CPU=vexriscv FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/c8e2e6d4b146d7a78273454b0aa9f6b9f740fe1e

---
+++ archive/master/v0.0.4-575-gc8e2e6d/mimas_a7/net/vexriscv/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMAS_A7
+#error "Version mismatch - PLATFORM_MIMAS_A7 not defined!"
+#endif
+const char* board = "mimas_a7";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "c8e2e6d4b146d7a78273454b0aa9f6b9f740fe1e";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-575-gc8e2e6d";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
