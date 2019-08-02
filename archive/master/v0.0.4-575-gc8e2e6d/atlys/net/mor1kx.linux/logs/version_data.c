59a0a28892fc7be4bf68dee140740bad35ef9b7c
TimVideos.us Robot <robot@timvideos.us>
2019-08-02 08:40:44 -0500

    Travis build #1175 of master/v0.0.4-575-gc8e2e6d for PLATFORM=atlys TARGET=net CPU=mor1kx.linux FIRMWARE=linux
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/c8e2e6d4b146d7a78273454b0aa9f6b9f740fe1e

---
+++ archive/master/v0.0.4-575-gc8e2e6d/atlys/net/mor1kx.linux/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "c8e2e6d4b146d7a78273454b0aa9f6b9f740fe1e";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-575-gc8e2e6d";
+const char* git_status =
+    "    --\r\n"
+   "    ?? ../../../../third_party/riscv-toolchain/\r\n"
+    "    --\r\n";
+
