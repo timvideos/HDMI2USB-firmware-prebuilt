5b883d7880c54f270076a07b1f640da4dac99f2c
TimVideos.us Robot <robot@timvideos.us>
2019-08-02 06:58:32 -0500

    Travis build #1175 of master/v0.0.4-575-gc8e2e6d for PLATFORM=opsis TARGET=net CPU=vexriscv FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/c8e2e6d4b146d7a78273454b0aa9f6b9f740fe1e

---
+++ archive/master/v0.0.4-575-gc8e2e6d/opsis/net/vexriscv/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "c8e2e6d4b146d7a78273454b0aa9f6b9f740fe1e";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-575-gc8e2e6d";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
