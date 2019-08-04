60cac1dd00034233e42a7325862f974b0ca26226
TimVideos.us Robot <robot@timvideos.us>
2019-08-03 22:17:31 -0500

    Travis build #1176 of master/v0.0.4-580-gfc388ff for PLATFORM=arty TARGET=base CPU=picorv32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/fc388fffa6e8ebd0c6483186c914177d7c69e7e6

---
+++ archive/master/v0.0.4-580-gfc388ff/arty/base/picorv32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "fc388fffa6e8ebd0c6483186c914177d7c69e7e6";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-580-gfc388ff";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
