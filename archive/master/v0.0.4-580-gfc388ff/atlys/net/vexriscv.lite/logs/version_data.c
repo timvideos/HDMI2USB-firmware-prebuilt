96ff8c32b5d15e1622dcd1899243a662ba0f3c7e
TimVideos.us Robot <robot@timvideos.us>
2019-08-04 00:10:12 -0500

    Travis build #1176 of master/v0.0.4-580-gfc388ff for PLATFORM=atlys TARGET=net CPU=vexriscv.lite FIRMWARE=zephyr
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/fc388fffa6e8ebd0c6483186c914177d7c69e7e6

---
+++ archive/master/v0.0.4-580-gfc388ff/atlys/net/vexriscv.lite/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "fc388fffa6e8ebd0c6483186c914177d7c69e7e6";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-580-gfc388ff";
+const char* git_status =
+    "    --\r\n"
+   "    ?? ../../../../third_party/zephyr/\r\n"
+    "    --\r\n";
+
