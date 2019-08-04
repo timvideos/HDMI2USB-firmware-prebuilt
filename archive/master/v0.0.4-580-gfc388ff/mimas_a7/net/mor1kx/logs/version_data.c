cba1ee2c75405211072d8755573417b383174644
TimVideos.us Robot <robot@timvideos.us>
2019-08-03 21:23:28 -0500

    Travis build #1176 of master/v0.0.4-580-gfc388ff for PLATFORM=mimas_a7 TARGET=net CPU=mor1kx FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/fc388fffa6e8ebd0c6483186c914177d7c69e7e6

---
+++ archive/master/v0.0.4-580-gfc388ff/mimas_a7/net/mor1kx/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMAS_A7
+#error "Version mismatch - PLATFORM_MIMAS_A7 not defined!"
+#endif
+const char* board = "mimas_a7";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "fc388fffa6e8ebd0c6483186c914177d7c69e7e6";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-580-gfc388ff";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
