6b35beae188c9fca94a519e312bcb45b6680514a
TimVideos.us Robot <robot@timvideos.us>
2019-10-25 04:44:42 -0500

    Travis build #1180 of master/v0.0.4-582-g5f50647 for PLATFORM=mimas_a7 TARGET=base CPU=picorv32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/5f5064789c1a579eb8c2dee2657c73c9c41a3638

---
+++ archive/master/v0.0.4-582-g5f50647/mimas_a7/base/picorv32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMAS_A7
+#error "Version mismatch - PLATFORM_MIMAS_A7 not defined!"
+#endif
+const char* board = "mimas_a7";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "5f5064789c1a579eb8c2dee2657c73c9c41a3638";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-582-g5f50647";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
