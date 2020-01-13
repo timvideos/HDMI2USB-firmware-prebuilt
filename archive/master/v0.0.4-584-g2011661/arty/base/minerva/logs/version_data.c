a90491dd604299d65d645656ee77f6484ef9184c
TimVideos.us Robot <robot@timvideos.us>
2020-01-12 20:48:14 -0600

    Travis build #1182 of master/v0.0.4-584-g2011661 for PLATFORM=arty TARGET=base CPU=minerva FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/20116612124b4479515578d01627fc4b182395fc

---
+++ archive/master/v0.0.4-584-g2011661/arty/base/minerva/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "20116612124b4479515578d01627fc4b182395fc";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-584-g2011661";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
