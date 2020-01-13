144e87dffb02ca54805606092dec21b9956184bb
TimVideos.us Robot <robot@timvideos.us>
2020-01-12 21:04:28 -0600

    Travis build #1182 of master/v0.0.4-584-g2011661 for PLATFORM=atlys TARGET=net CPU=mor1kx.linux FIRMWARE=linux
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/20116612124b4479515578d01627fc4b182395fc

---
+++ archive/master/v0.0.4-584-g2011661/atlys/net/mor1kx.linux/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "20116612124b4479515578d01627fc4b182395fc";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-584-g2011661";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
