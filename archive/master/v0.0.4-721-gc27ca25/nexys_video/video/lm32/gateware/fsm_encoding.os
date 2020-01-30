acca7df4f954db50366b2c88dc3a3016e0feb6d4
TimVideos.us Robot <robot@timvideos.us>
2020-01-30 10:35:53 -0600

    Travis build #1183 of master/v0.0.4-721-gc27ca25 for PLATFORM=nexys_video TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/c27ca257f49e580183222aa2b10623ae6c544553

---
+++ archive/master/v0.0.4-721-gc27ca25/nexys_video/video/lm32/gateware/fsm_encoding.os
@@ -0,0 +1,15 @@
+
+ add_fsm_encoding \
+       {top.vns_refresher_state} \
+       { }  \
+       {{00 00} {01 01} {10 10} {11 11} }
+
+ add_fsm_encoding \
+       {top.vns_oled_state} \
+       { }  \
+       {{00 00} {01 01} {10 10} {11 11} }
+
+ add_fsm_encoding \
+       {top.vns_dma_state} \
+       { }  \
+       {{00 00} {01 01} {10 10} {11 11} }
---
+++ archive/master/v0.0.4-721-gc27ca25/nexys_video/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "c27ca257f49e580183222aa2b10623ae6c544553";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-721-gc27ca25";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
