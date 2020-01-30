ff11d006de768aab1fcc7296b7ac6f923bed11ce
TimVideos.us Robot <robot@timvideos.us>
2020-01-30 10:34:42 -0600

    Travis build #1183 of master/v0.0.4-721-gc27ca25 for PLATFORM=arty TARGET=net CPU=vexriscv FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/c27ca257f49e580183222aa2b10623ae6c544553

---
+++ archive/master/v0.0.4-721-gc27ca25/arty/net/vexriscv/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "c27ca257f49e580183222aa2b10623ae6c544553";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-721-gc27ca25";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
